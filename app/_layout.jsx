2cc3ed08488587fc62d315513c0f973e940de648
ilian <ilian.gladyshev@gmail.com>
2026-03-07 16:43:17 -0500

    Added the first transition

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,21 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
-import { Stack } from 'expo-router'
+import { Stack, useRouter } from 'expo-router'
 import { layout_styles } from '../styles/pages/layout'
 import { ui_elements_styles } from '../styles/ui_elements'
 
 const _layout = () => {
+    const router = useRouter();
+
     return (
         <View style={layout_styles.background}>
             <SafeAreaView style={layout_styles.headerContainer} edges={['top']}>
                 <Text>SecondChance</Text>
-                <Pressable style={({ pressed }) => [ui_elements_styles.infoButton, ui_elements_styles.button, pressed && ui_elements_styles.buttonPressed]}>
+                <Pressable 
+                    onPress={() => router.push('/info')}
+                    style={({ pressed }) => [ui_elements_styles.infoButton, ui_elements_styles.button, pressed && ui_elements_styles.buttonPressed]}
+                >
                     <Text style={ui_elements_styles.infoText}>More Info</Text>
                 </Pressable>
             </SafeAreaView>
